481250adaea55d0e076529559020d0a144061c60
kuviman <kuviman@gmail.com>
2024-10-04 22:21:59 +0400

    reorder test code

--- tests/count-tree-nodes.rs
+++ tests/count-tree-nodes.rs
@@ -5,28 +5,6 @@ struct Tree {
     children: Vec<Tree>,
 }
 
-impl Drop for Tree {
-    fn drop(&mut self) {
-        if self.children.is_empty() {
-            return;
-        }
-        async fn safe_drop(mut tree: Tree) {
-            for child in std::mem::take(&mut tree.children) {
-                safe_drop(child).decurse().await;
-            }
-        }
-        run_decursing(safe_drop(std::mem::take(self)));
-    }
-}
-
-async fn total_nodes(mut tree: Tree) -> usize {
-    let mut result = 1;
-    for child in std::mem::take(&mut tree.children) {
-        result += total_nodes(child).decurse().await;
-    }
-    result
-}
-
 fn recursive_total_nodes(mut tree: Tree) -> usize {
     let mut result = 1;
     for child in std::mem::take(&mut tree.children) {
@@ -36,7 +14,6 @@ fn recursive_total_nodes(mut tree: Tree) -> usize {
 }
 
 const N: usize = 1_000_000;
-
 fn construct_tree() -> Tree {
     let mut tree = Tree::default();
     for _ in 0..N {
@@ -54,8 +31,30 @@ fn test_overflow() {
     assert_eq!(recursive_total_nodes(tree), N * 2 + 1);
 }
 
+async fn total_nodes(mut tree: Tree) -> usize {
+    let mut result = 1;
+    for child in std::mem::take(&mut tree.children) {
+        result += total_nodes(child).decurse().await;
+    }
+    result
+}
+
 #[test]
 fn test_decursed() {
     let tree = construct_tree();
     assert_eq!(run_decursing(total_nodes(tree)), N * 2 + 1);
+}
+
+impl Drop for Tree {
+    fn drop(&mut self) {
+        if self.children.is_empty() {
+            return;
+        }
+        async fn safe_drop(mut tree: Tree) {
+            for child in std::mem::take(&mut tree.children) {
+                safe_drop(child).decurse().await;
+            }
+        }
+        run_decursing(safe_drop(std::mem::take(self)));
+    }
 }
