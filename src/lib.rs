d90fddf43f81df376510c4134158b4b5944b4965
kuviman <kuviman@gmail.com>
2024-10-04 17:34:47 +0400

    Proof of concept

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use future::LocalBoxFuture;
+use futures::prelude::*;
+use scoped_tls_hkt::scoped_thread_local;
+use std::cell::RefCell;
+use std::future::Future;
+use std::rc::Rc;
+
+pub struct Decurser {
+    call_stack: Vec<Rc<RefCell<LocalBoxFuture<'static, ()>>>>,
+}
+
+pub fn run_decursing<T: 'static>(f: impl Future<Output = T> + 'static) -> T {
+    let (mut sender, receiver) = async_oneshot::oneshot();
+    let decurser = RefCell::new(Decurser {
+        call_stack: vec![Rc::new(RefCell::new(
+            f.map(move |result| sender.send(result).unwrap())
+                .boxed_local(),
+        ))],
+    });
+    DECURSER.set(&decurser, || {
+        let waker = futures::task::noop_waker();
+        let mut cx = std::task::Context::from_waker(&waker);
+        loop {
+            let decurser_ref = decurser.borrow();
+            let Some(f) = decurser_ref.call_stack.last() else {
+                break;
+            };
+            let f = Rc::clone(f);
+            std::mem::drop(decurser_ref);
+
+            match f.borrow_mut().poll_unpin(&mut cx) {
+                std::task::Poll::Ready(()) => {
+                    // the innermost function in the call stack has exited
+                    decurser.borrow_mut().call_stack.pop();
+                }
+                std::task::Poll::Pending => {
+                    // this should mean that we have called inner function with decurse
+                }
+            };
+        }
+        receiver.try_recv().map_err(|_| ()).unwrap()
+    })
+}
+
+scoped_thread_local!(static DECURSER: RefCell<Decurser>);
+
+pub struct RecursedFuture<T>(async_oneshot::Receiver<T>);
+
+impl<T> Future for RecursedFuture<T> {
+    type Output = T;
+
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        unsafe {
+            self.map_unchecked_mut(|this| &mut this.0)
+                .poll(cx)
+                .map(|result| result.expect("the sender was dropped???"))
+        }
+    }
+}
+
+pub trait FutureExt: Future {
+    fn decurse(self) -> LocalBoxFuture<'static, Self::Output>;
+}
+
+impl<F: Future> FutureExt for F
+where
+    Self: 'static,
+{
+    fn decurse(self) -> LocalBoxFuture<'static, Self::Output> {
+        let (mut sender, receiver) = async_oneshot::oneshot();
+        if !DECURSER.is_set() {
+            panic!("You can only decurse when inside a decursing context");
+        }
+        DECURSER.with(|decurser| {
+            let mut decurser = decurser.borrow_mut();
+            decurser.call_stack.push(Rc::new(RefCell::new(Box::pin(
+                self.map(move |result| sender.send(result).unwrap()),
+            ))));
+        });
+        receiver.map(|result| result.unwrap()).boxed_local()
+    }
+}
--- tests/dfs.rs
+++ tests/dfs.rs
@@ -1,15 +1,52 @@
-enum Tree {
-    Leaf,
-    InnerNode(Vec<Tree>),
+use decursion::*;
+
+struct Children(Vec<Tree>);
+
+impl From<Vec<Tree>> for Children {
+    fn from(value: Vec<Tree>) -> Self {
+        Self(value)
+    }
 }
 
-fn total_nodes(tree: &Tree) -> usize {
+impl Drop for Children {
+    fn drop(&mut self) {
+        std::mem::forget(std::mem::take(&mut self.0)); // TODO dont leak
+    }
+}
+
+impl IntoIterator for Children {
+    type Item = Tree;
+    type IntoIter = Box<dyn Iterator<Item = Tree>>;
+    fn into_iter(mut self) -> Self::IntoIter {
+        Box::new(std::mem::take(&mut self.0).into_iter())
+    }
+}
+
+enum Tree {
+    Leaf,
+    InnerNode(Children),
+}
+
+async fn total_nodes(tree: Tree) -> usize {
     match tree {
         Tree::Leaf => 1,
         Tree::InnerNode(children) => {
             let mut result = 1;
             for child in children {
-                result += total_nodes(child);
+                result += total_nodes(child).decurse().await;
+            }
+            result
+        }
+    }
+}
+
+fn broken_total_nodes(tree: Tree) -> usize {
+    match tree {
+        Tree::Leaf => 1,
+        Tree::InnerNode(children) => {
+            let mut result = 1;
+            for child in children {
+                result += broken_total_nodes(child);
             }
             result
         }
@@ -17,11 +54,22 @@ fn total_nodes(tree: &Tree) -> usize {
 }
 
 #[test]
-fn test_dfs() {
+#[ignore = "this test stack overflows"]
+fn test_broken() {
     let mut tree = Tree::Leaf;
     const N: usize = 1_000_000;
     for _ in 0..N {
-        tree = Tree::InnerNode(vec![tree, Tree::Leaf]);
+        tree = Tree::InnerNode(vec![tree, Tree::Leaf].into());
     }
-    assert_eq!(total_nodes(&tree), N * 2 + 1);
+    assert_eq!(broken_total_nodes(tree), N * 2 + 1);
+}
+
+#[test]
+fn test_decursed() {
+    let mut tree = Tree::Leaf;
+    const N: usize = 1_000_000;
+    for _ in 0..N {
+        tree = Tree::InnerNode(vec![tree, Tree::Leaf].into());
+    }
+    assert_eq!(run_decursing(total_nodes(tree)), N * 2 + 1);
 }
